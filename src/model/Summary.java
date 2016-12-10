7a759562c60c52e96a72d709b6c884d3bac69f6f
Michael Limbird <m-limbird@onu.edu>
2016-12-10 18:10:53 -0500

    Added accessors and mutators to Summary.java in src/model.

--- src/model/Summary.java
+++ src/model/Summary.java
@@ -6,10 +6,10 @@ package model;
  *
  */
 public class Summary {
-	public int total;
-	public int covered;
-	public int live;
-	public int killed;
+	private int total;
+	private int covered;
+	private int live;
+	private int killed;
 	
 	public Summary(int total, 
 				   int covered, 
@@ -24,4 +24,41 @@ public class Summary {
 	public boolean hasLive() {
 		return killed != total;
 	}
+
+
+	//Accessors
+	public int getTotal(){
+		return this.total;
+	}
+
+	public int getCovered(){
+		return this.covered;
+	}
+
+	public int getLive(){
+		return this.live;
+	}
+
+	public int getKilled(){
+		return this.killed;
+	}
+
+
+	//Mutators
+	public void setTotal(int newTotal){
+		this.total = newTotal;
+	}
+
+	public void setCovered(int newCovered){
+		this.covered = newCovered;
+	}
+
+	public void setLive(int newLive){
+		this.live = newLive;
+	}
+
+	public void setKilled(int newKilled){
+		this.killed = newKilled;
+	}
+
 }
