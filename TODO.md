6024c9e6447c0f26663e51f228be4853f899a4d8
Michael Limbird <m-limbird@onu.edu>
2016-12-11 15:48:56 -0500

    Added new tests to the MutatorTest, but it really needs to have the updateSummary test case changed. Added notes about what needs to be done in TODO

--- TODO.md
+++ TODO.md
@@ -7,3 +7,10 @@ Mutant only has addTest capabilities. Does it need deleteTest capabilities?
 Do we need tests for each accessor and mutator?
 
 Need to add javadoc comments to all functions?
+
+In Mutator.java:
+	 method updateSummary: It doesn't appear that the MutatorVizModel adds any functionality to the method. Why is it an argument to this method?
+
+	 Looks like the updateSummary would also make any "unknown" status mutants into killed. This doesn't seem right to me. I will add a branch to do nothing if it equals "unknown";
+
+	 Actually, it looks like this was supposed to use the model.mutants, but since the class already has a mutants field, it accepted this code - although I believe it is not correct.
--- test/model/MutatorTest.java
+++ test/model/MutatorTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Test class for the Mutator implementation.
@@ -11,14 +12,87 @@ import java.util.ArrayList;
  */
 public class MutatorTest {
 
-    /*
-     * Test the 
+    /**
+     * Test the constructor of MutatorTest
      */
     @Test
-    public void test1() {
-        // Type actual = Triangle.classify(1, 1, 1);
-        // Type expected = EQUILATERAL;
-        // assertEquals (expected, actual);
-        assert(true);
+    public void testConstructor() {
+        
+        //Initialize the Mutator
+        Mutator m = new Mutator("newName");
+
+        //Check the Name
+        String actualName = m.getName();
+        String expectedName = "newName";
+        assertEquals(expectedName, actualName);
+
+        //Check the Summary
+        Summary actualSummary = m.getSummary();
+        String expectedSummary = new Summary(0,0,0,0);
+        assertEquals(expectedSummary, actualSummary);
+
+        //Check the Mutants
+        List<Mutant> actualMutants = m.getMutants();
+        List<Mutant> expectedMutants = new ArrayList<Mutant>();
+        assertEquals(expectedMutants, actualMutants);
     }
+
+    @Test
+    public void testAddMutant() {
+        
+        //Initialize the Mutator
+        Mutator mt = new Mutator("newName");
+
+        //Initialize a new Mutant
+        Mutant m = new Mutant(2, LVR, "class", "newName", 3, "newSource");
+
+        //Check adding the mutant
+        mt.addMutant(m);
+        List<Mutant> actualMutants = m.getMutants();
+        List<Mutant> expectedMutants = new ArrayList<Mutant>();
+        expectedMutants.add(m);
+        assertEquals(expectedMutants, actualMutants);
+
+    }
+
+    @Test
+    public void testUpdateSummary() {
+        
+        //Initialize the Mutator
+        Mutator mt = new Mutator("newName");
+
+        //Initialize a mutant to add
+        Mutant m = new Mutant(2, LVR, "class", "newName", 3, "newSource");
+
+        //Initialize the Model to test with
+        TriangleModel triMod = new TriangleModel();
+
+        //Check the updateSummary without adding mutant
+        m.updateSummary(triMod);
+
+
+
+        //Check the updateSummary with a new mutant
+        mt.addMutant(m);
+        assert(false);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
