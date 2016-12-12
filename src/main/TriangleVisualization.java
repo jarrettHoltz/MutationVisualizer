67dfd5a2212f5b8b3e882bf3dc56c8d251de135e
Rafael Lizarralde <rezecib@gmail.com>
2016-12-12 16:53:57 -0500

    Fixed top-level JTree nodes missing on Macs

--- src/main/TriangleVisualization.java
+++ src/main/TriangleVisualization.java
@@ -1,7 +1,10 @@
 package main;
 
+import java.awt.Font;
+import java.util.Set;
+
 import javax.swing.ToolTipManager;
-import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.UIManager;
 
 import controller.TriangleParser;
 import model.TriangleModel;
@@ -13,6 +16,15 @@ public class TriangleVisualization {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+	    try {
+			// Set cross-platform Java L&F (also called "Metal")
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+	    } 
+	    catch (Exception e) {
+			//This should really never happen
+			e.printStackTrace();
+	    }
+
 		TriangleModel model = new TriangleModel();
 		TriangleParser parser = new TriangleParser();
 		parser.BuildModel(model,"triangle/mutation_results", "triangle/src", "triangle/test" );
@@ -20,5 +32,4 @@ public class TriangleVisualization {
 		//Make hovering over summary bar segments more responsive
 		ToolTipManager.sharedInstance().setInitialDelay(100); //Default is 750ms
 	}
-
 }
