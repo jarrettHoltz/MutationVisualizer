7ef28b1061eafced2714276202ae47423567c6aa
Michael Limbird <m-limbird@onu.edu>
2016-12-12 16:15:32 -0500

    fixed build file to allow for testing again.

--- test/model/TriangleModelTest.java
+++ test/model/TriangleModelTest.java
@@ -20,6 +20,33 @@ public class TriangleModelTest {
         //Initialize the TriangleModel
         TriangleModel tm = new TriangleModel();
 
-        //
+        //Check the sources
+
+        //Check the tests
+
+        //Check the mutants
+
+        //Check the mutator map
+
+        //Check the summary
+
+    }
+
+    @Test
+    public void testSetSummary() {
+        
+        //Initialize the TriangleModel
+        TriangleModel tm = new TriangleModel();
+
+        Summary expectedSummary = new Summary(4,3,2,1);
+        tm.setSummary(expectedSummary);
+        Summary actualSummary = tm.getSummary();
+
+        assertEquals(expectedSummary.getTotal(), actualSummary.getTotal());
+        assertEquals(expectedSummary.getCovered(), actualSummary.getCovered());
+        assertEquals(expectedSummary.getLive(), actualSummary.getLive());
+        assertEquals(expectedSummary.getKilled(), actualSummary.getKilled());
+
+        
     }
 }
